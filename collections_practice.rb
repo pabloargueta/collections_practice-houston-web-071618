c91d9d264337f85790fea2b3b9273a014303e2b9
pabloargueta <pabloargueta27@gmail.com>
2018-07-17 23:36:07 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,59 @@
+require 'pry'
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|x,y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort {|x,y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+
+  array.map {|string|
+    string_array = string.split('')
+    string_array[2] = '$'
+    string_array= string_array.join('')
+  }
+
+end
+
+def find_a(array)
+  array.select {|string|
+  string.start_with?("a")
+}
+end
+
+
+def sum_array(array)
+
+  sum = 0
+  array.each {|num|
+      sum += num
+  }
+  sum
+
+end
+
+def add_s(array)
+  array.each_with_index.collect {|element, index|
+    if index != 1
+      element += 's'
+    else
+      element
+    end
+  }
+
+end
